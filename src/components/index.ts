02550e3703afd00a198388ec4f71ba70ff607427
Houthoofd <houthoofd.benoit48@gmail.com>
2023-09-16 15:31:27 +0200

    prepare merge

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import { CustomElement } from 'thorium-framework';
+
+export type CustomMouseEvent = MouseEvent & { target : CustomElementTarget };
+export type CustomElementTarget = CustomElement<Element,({} & CustomElementDefultProps)>
+
+export type CustomElementDefultProps = {
+  id?:string;
+  name?:string;
+  className?:string;
+}
