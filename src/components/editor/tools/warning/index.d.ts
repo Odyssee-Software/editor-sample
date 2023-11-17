4aedd28dab4dc22aa97f668cf8d0c3b5512ec826
Houthoofd <houthoofd.benoit48@gmail.com>
2023-11-17 17:58:43 +0100

    Update

--- src/components/editor/tools/warning/index.d.ts
+++ src/components/editor/tools/warning/index.d.ts
@@ -1,7 +1,7 @@
 export declare class Warning {
     context: import("thorium-store-context/dist/store-context").IStoreContext;
     valueStateManager: void | import("thorium-store-context/dist/store-context").IStoreState<string>;
-    typeStateManager: void | import("thorium-store-context/dist/store-context").IStoreState<"message" | "warning" | "alert">;
+    typeStateManager: void | import("thorium-store-context/dist/store-context").IStoreState<"warning" | "message" | "alert">;
     constructor();
     static get settings(): {
         message: string;
