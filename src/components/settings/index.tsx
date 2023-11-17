dcbc8782594f595baa2b600be3372126dda54ace
Houthoofd <houthoofd.benoit48@gmail.com>
2023-11-17 15:58:44 +0100

    Update

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -5,27 +5,27 @@ import styles from './style.module.css';
 
 export const Settings = () => {
 
-    let state = useState<string[]>([ "un" , "deux" ]);
+  let state = useState<string[]>('' , [ "un" , "deux" ]);
 
-    return <div class={styles.Settings}>
-      <div class={styles.SideLeftMenu}>
+  return <div class={styles.Settings}>
+    <div class={styles.SideLeftMenu}>
 
+    </div>
+    <div class={styles.Content}>
+      <div class={styles.ContentHeader}>
+          
       </div>
-      <div class={styles.Content}>
-        <div class={styles.ContentHeader}>
-            
-        </div>
-        <div class={styles.MainContentHeader}>
-            <h3>Settings</h3>
-            <div class={styles.ButtonHome}>
-                <Button textContent='Home' pageLink = {{ to : '/' }} />
-            </div>
-        </div>
-        <div class={styles.MainContent}>
-            <div class={styles.ListItem}>
-                < WrapperList items= {state}/>
-            </div>
-        </div>
+      <div class={styles.MainContentHeader}>
+          <h3>Settings</h3>
+          <div class={styles.ButtonHome}>
+              <Button textContent='Home' pageLink = {{ to : '/' }} />
+          </div>
       </div>
-    </div>;
+      <div class={styles.MainContent}>
+          <div class={styles.ListItem}>
+              < WrapperList items= {state}/>
+          </div>
+      </div>
+    </div>
+  </div>;
 }
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,7 @@
 import { PageRouter, Page, PageLink } from "thorium-framework";
 import { Workspace } from '@components/workspace';
+import { Settings } from '@components/settings';
+
 const Home = () => {
     return <div>
     <h1>Home</h1>
@@ -7,12 +9,7 @@ const Home = () => {
     <PageLink to="/settings" title="settings"/>
   </div>;
 };
-const Settings = () => {
-    return <div>
-    <h1>Settings</h1>
-    <PageLink to="/" title="home"/>
-  </div>;
-};
+
 /* The code is exporting a default component that uses the `PageRouter` and `Page` components from the
 "thorium-framework" library. */
 export default <PageRouter>
