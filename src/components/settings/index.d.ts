90bb474b768a6416470a40b16588b6e37f69e7ea
Houthoofd <houthoofd.benoit48@gmail.com>
2023-11-17 17:59:41 +0100

    Update

--- src/components/settings/index.d.ts
+++ src/components/settings/index.d.ts
@@ -1,11 +1,1 @@
-import { State } from 'thorium-framework';
-interface ISettings {
-}
-export type SettingsStateMutator = [State<ISettings>, (value: ISettings) => ISettings];
-export interface SettingsButtonProps {
-    settingsStateMutator: SettingsStateMutator;
-    action?(event: MouseEvent): void;
-}
-export declare const SettingsButton: (props: SettingsButtonProps) => any;
 export declare const Settings: () => any;
-export {};
